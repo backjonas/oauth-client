e685ba7b8d6a6ba11c3b0f37b2576a252c172531
Jonas Bäck <jonas.c.back@gmail.com>
2024-04-03 09:43:20 +0300

    Add initial OAuth authorization code flow using Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { HomePage } from './pages/HomePage'
+import { OAuthCallbackPage } from './pages/OAuthCallbackPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/code" element={<OAuthCallbackPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
