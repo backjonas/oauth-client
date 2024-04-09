1c31ba0aa9e7b26daeb99774291abe8626e0a454
Jonas Bäck <jonas.c.back@gmail.com>
2024-04-09 15:16:37 +0300

    Remove react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { HomePage } from './pages/HomePage'
-import { OAuthCallbackPage } from './pages/OAuthCallbackPage'
+import { useEffect, useState } from 'react'
 
-function App() {
+const API_URL = import.meta.env.API_URL || 'http://localhost:3001'
+
+const App = () => {
+  const [email, setEmail] = useState('')
+  useEffect(() => {
+    const fetchEmail = async () => {
+      const emailResponse = await fetch(`${API_URL}/oauth/email`, {
+        credentials: 'include',
+      })
+      if (!emailResponse.ok) {
+        return
+      }
+
+      const responseObject = await emailResponse.json()
+      if ('email' in responseObject) {
+        setEmail(responseObject.email)
+      }
+    }
+    fetchEmail()
+  })
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/code" element={<OAuthCallbackPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>OAuth 2.0 client demo</h1>
+      <div className="card">
+        <button
+          onClick={() => (window.location.href = `${API_URL}/oauth/login`)}
+        >
+          Login
+        </button>
+        <p>
+          {email === ''
+            ? 'User not logged in'
+            : `User logged in with email: ${email}`}
+        </p>
+      </div>
+    </>
   )
 }
 
